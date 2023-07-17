8699866c5c00372b20a65063ad8dfdb1652cda70
Kosuke <tirol.developer@gmail.com>
2023-07-17 16:59:16 +0900

    Chapter 7.4

--- sample_app/test/integration/users_signup_test.rb
+++ sample_app/test/integration/users_signup_test.rb
@@ -1,7 +1,6 @@
 require "test_helper"
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-
   test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
@@ -14,5 +13,17 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_template 'users/new'
     assert_select 'div#error_explanation'
     assert_select 'div.alert-danger'
+  end
+
+  test "valid signup information" do
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name:  "Example User",
+                                         email: "user@example.com",
+                                         password:              "password",
+                                         password_confirmation: "password" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.nil?
   end
 end
