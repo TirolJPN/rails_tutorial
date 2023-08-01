077e9832915f88ab3cf3f8233a1a3824135c2903
Kosuke <tirol.developer@gmail.com>
2023-08-02 01:44:52 +0900

    12.3.3 の手直し

--- sample_app/test/integration/password_resets_test.rb
+++ sample_app/test/integration/password_resets_test.rb
@@ -1,12 +1,14 @@
 require "test_helper"
 
 class PasswordResets < ActionDispatch::IntegrationTest
+
   def setup
     ActionMailer::Base.deliveries.clear
   end
 end
 
 class ForgotPasswordFormTest < PasswordResets
+
   test "password reset path" do
     get new_password_reset_path
     assert_template 'password_resets/new'
@@ -22,15 +24,18 @@ class ForgotPasswordFormTest < PasswordResets
 end
 
 class PasswordResetForm < PasswordResets
+
   def setup
+    super
     @user = users(:michael)
     post password_resets_path,
-      params: {password_reset: {email: @user.email}}
-      @reset_user = assigns(:user)
+         params: { password_reset: { email: @user.email } }
+    @reset_user = assigns(:user)
   end
 end
 
 class PasswordFormTest < PasswordResetForm
+
   test "reset with valid email" do
     assert_not_equal @user.reset_digest, @reset_user.reset_digest
     assert_equal 1, ActionMailer::Base.deliveries.size
@@ -45,7 +50,9 @@ class PasswordFormTest < PasswordResetForm
 
   test "reset with inactive user" do
     @reset_user.toggle!(:activated)
-    get edit_password_reset_path(@reset_user.reset_token)
+    get edit_password_reset_path(@reset_user.reset_token,
+                                 email: @reset_user.email)
+    assert_redirected_to root_url
   end
 
   test "reset with right email but wrong token" do
@@ -54,28 +61,36 @@ class PasswordFormTest < PasswordResetForm
   end
 
   test "reset with right email and right token" do
-    get edit_password_reset_path(@reset_user.reset_token, email: @reset_user.email)
+    get edit_password_reset_path(@reset_user.reset_token,
+                                 email: @reset_user.email)
     assert_template 'password_resets/edit'
     assert_select "input[name=email][type=hidden][value=?]", @reset_user.email
   end
 end
 
 class PasswordUpdateTest < PasswordResetForm
+
   test "update with invalid password and confirmation" do
     patch password_reset_path(@reset_user.reset_token),
-        params: {email: @reset_user.email, user: {password: "foobaz", password_confirmation: "barquux"}}
+          params: { email: @reset_user.email,
+                    user: { password:              "foobaz",
+                            password_confirmation: "barquux" } }
     assert_select 'div#error_explanation'
   end
 
   test "update with empty password" do
     patch password_reset_path(@reset_user.reset_token),
-        params: {email: @reset_user.email, user: {password: "", password_confirmation: ""}}
+          params: { email: @reset_user.email,
+                    user: { password:              "",
+                            password_confirmation: "" } }
     assert_select 'div#error_explanation'
   end
 
   test "update with valid password and confirmation" do
     patch password_reset_path(@reset_user.reset_token),
-        params: {email: @reset_user.email, user: {password: "foobaz", password_confirmation: "foobaz"}}
+          params: { email: @reset_user.email,
+                    user: { password:              "foobaz",
+                            password_confirmation: "foobaz" } }
     assert is_logged_in?
     assert_not flash.empty?
     assert_redirected_to @reset_user
